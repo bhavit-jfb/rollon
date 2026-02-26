4ff48c62a51d95a5ca90204064fe8d564b3e94c3
bhavit-jfb <jfbrecov@gmail.com>
2026-02-26 14:31:08 +0530

    Update main.py

--- Python-Website-Using-Flask-main/main.py
+++ Python-Website-Using-Flask-main/main.py
@@ -9,7 +9,7 @@ from flask_socketio import SocketIO, emit
 import os
 
 app = Flask(__name__, template_folder="templates")
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 socketio = SocketIO(app)
 
 # Configure the SQLite databases
@@ -18,7 +18,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # app.config['SQLALCHEMY_BINDS'] = {
 #     'users': 'sqlite:///' + os.path.join(basedir, 'users.db')
 # }
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -236,8 +236,8 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 # app.config['MAIL_USERNAME'] = 'jfbrecov@gmail.com'
 # app.config['MAIL_PASSWORD'] = 'xnrvemhkolarjhye'
-app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
-app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = 'jfbrecov@gmail.com'
 
 mail = Mail(app)
@@ -300,8 +300,9 @@ def forbidden(e):
     return render_template("403.html"), 403
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
-
+with app.app_context():
+    db.create_all()
 
 #app.run(debug=True)
