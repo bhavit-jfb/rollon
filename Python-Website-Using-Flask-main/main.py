8015a0c76dd97958c6ba19a7ae248f9cca94cb11
bhavit-jfb <jfbrecov@gmail.com>
2026-02-26 14:33:39 +0530

    Update main.py

--- Python-Website-Using-Flask-main/main.py
+++ Python-Website-Using-Flask-main/main.py
@@ -18,7 +18,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # app.config['SQLALCHEMY_BINDS'] = {
 #     'users': 'sqlite:///' + os.path.join(basedir, 'users.db')
 # }
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+database_url = os.environ.get('DATABASE_URL')
+
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
